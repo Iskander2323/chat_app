67d69b384b0ccec7500ea63822128a2213b79e31
Iskander2323 <iska200314@gmail.com>
2025-05-05 19:31:55 +0500

    Create app_color.dart

---
+++ lib/components/custom/app_color.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColor {
+  static const primary = Color.fromRGBO(165, 47, 191, 1);
+  static const backgroundColor = Color.fromRGBO(191, 191, 191, 1);
+  static const white = Color.fromRGBO(255, 255, 255, 1);
+  static const textWhite = Color.fromRGBO(255, 255, 255, 1);
+  static const textBlack = Color.fromRGBO(0, 0, 0, 1);
+  static const messageHintBackgroundColor = Color.fromRGBO(0, 159, 59, 1);
+}
