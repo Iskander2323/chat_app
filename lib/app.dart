3cb1a911d5b61e3cc12b3c540c884e38d102964f
Iskander2323 <iska200314@gmail.com>
2025-05-06 17:58:44 +0500

    added chat api

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,25 @@
-import 'package:chat_app/components/firebase_api/firebase_api.dart';
+import 'package:chat_app/components/firebase_api/firebase_auth_api.dart';
+import 'package:chat_app/components/firebase_api/firebase_chat_api.dart';
 import 'package:chat_app/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
-  const App({super.key, required this.firebaseApi});
+  const App({
+    super.key,
+    required this.firebaseApi,
+    required this.firebaseChatApi,
+  });
 
-  final FirebaseApi firebaseApi;
+  final FirebaseAuthApi firebaseApi;
+  final FirebaseChatApi firebaseChatApi;
 
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
-        RepositoryProvider<FirebaseApi>(create: (context) => firebaseApi),
+        RepositoryProvider<FirebaseAuthApi>(create: (context) => firebaseApi),
+        RepositoryProvider(create: (context) => firebaseChatApi),
       ],
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/app.dart';
-import 'package:chat_app/components/firebase_api/firebase_api.dart';
+import 'package:chat_app/components/firebase_api/firebase_auth_api.dart';
+import 'package:chat_app/components/firebase_api/firebase_chat_api.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  final FirebaseApi firebaseApi = FirebaseApi();
+  final FirebaseAuthApi firebaseApi = FirebaseAuthApi();
+  final FirebaseChatApi firebaseChatApi = FirebaseChatApi();
 
-  runApp(App(firebaseApi: firebaseApi));
+  runApp(App(firebaseApi: firebaseApi, firebaseChatApi: firebaseChatApi));
 }
